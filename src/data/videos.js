8e260a95fa8e369395cde4ff875b823ca2bb09b6
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-07-31 15:20:27 -0300

    Update videos

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,58 +1,19 @@
 const videoList = [
   {
-    title: "Meet Daisy, Student",
-    description: "Student Testimonial",
-    videoId: "SQ8jtcPgOyA",
+    title: "FlyTech Pilot Academy",
+    description: "About FlyTech Pilot Academy",
+    videoId: "0mwjkKUwlI8",
   },
   {
-    title: "Come Fly With Us",
-    description: "Content",
-    videoId: "RFfQdkE2F2c",
+    title: "Lane Hall Solo Flight Training",
+    description:
+      "Lane Hall Solo in FlyTech Pilot Academy Sling NGT also known as Sling 2 Special LSA",
+    videoId: "FSliFHlpze0",
   },
   {
-    title: "Meet Zach, Chief Pilot",
-    description: "Instructor Testimonial",
-    videoId: "gp2HESFh4IY",
-  },
-  {
-    title: "Meet Mitch, Flight Instructor",
-    description: "Instructor Testimonial",
-    videoId: "OQikPnwRgw",
-  },
-  {
-    title: "Meet Devon, Flight Instructor",
-    description: "Instructor Testimonial",
-    videoId: "MQyGBr4dSjE",
-  },
-  {
-    title: "Pre Flight Check with Josh",
-    description: "Content with audio",
-    videoId: "gulhBmBnj00",
-  },
-  {
-    title: "FlyTech Flight School",
-    description: "Content",
-    videoId: "dgzgM142d0k",
-  },
-  {
-    title: "Funding Your Flight Training",
-    description: "Financing Info",
-    videoId: "_BlgEr4MPI0",
-  },
-  {
-    title: "Schedule a discovery flight today!",
-    description: "Content",
-    videoId: "PGxJdb2pSPk",
-  },
-  {
-    title: "Checklist Chats",
-    description: "Student Experience",
-    videoId: "QjjdVwPR6XA",
-  },
-  {
-    title: "Chandler Close Testimonial",
-    description: "Student Testimonial",
-    videoId: "mepFv2sCA_0",
+    title: "Take Off and Adhoc IFR Setup",
+    description: "Take off and adhoc IFR Setup",
+    videoId: "Gh59IkM3O1M",
   },
 ];
 
