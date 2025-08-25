5a3ccd1091279c71484fd703be1fe28c5a2713ff
Lucas Rodrigues <lcrodrigues1995@gmail.com>
2025-08-25 18:08:16 -0300

    Update programSwiper.js

--- src/data/flight-training/programSwiper.js
+++ src/data/flight-training/programSwiper.js
@@ -10,7 +10,7 @@ const programsSwiper = {
       imageAlt:
         "FlyTech Pilot Academy student and flight instructor in a Cessna airplane",
       description:
-        "Your passport to the skies! Learn to pilot your dreams with our Private Pilot Course.",
+        "Fly higher, farther, and with more passengers, gain the skills to command cross-country flights with confidence.",
       link: "/flight-training/private-pilot",
     },
     {
@@ -40,12 +40,12 @@ const programsSwiper = {
       link: "/flight-training/certified-flight-instructor",
     },
     {
-      title: "Certified Flight Instructor Instruments",
+      title: "Certified Flight Instructor Instrument",
       imagePath: "/src/assets/cockpit-flying-sunset.webp",
       imageAlt:
         "FlyTech Pilot Academy student and flight instructor in a Cessna airplane cockpit",
       description:
-        "Become a trusted guide in the world of aviation. Become a CFI and help others reach their dreams.",
+        "Become a trusted guide in the world of aviation. Become a CFII and help others reach their dreams.",
       link: "/flight-training/certified-flight-instructor-instruments",
     },
     {
@@ -54,7 +54,7 @@ const programsSwiper = {
       imageAlt:
         "FlyTech Pilot Academy student and flight instructor in a Cessna airplane cockpit",
       description:
-        "Created for people who desire to fly one or two seat aircraft that are smaller, lighter, and easier to fly. ",
+        "Created for people who desire to fly recreationally. ",
       link: "/flight-training/sports-pilot-certificate",
     },
   ],
