137a76c3461ab206d34c0890d8a3ff30dd9c613a
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-09-04 18:38:27 -0300

    Add new event component

---
+++ src/data/event-sessions.js
@@ -0,0 +1,16 @@
+const groundSchoolSessions = [
+  {
+    date: "April 19, 2025",
+    time: "8:00 AM – 11:00 AM",
+    topics: ["Pilot Qualifications", "Airworthiness"],
+    status: "Scheduled",
+  },
+  {
+    date: "TBD",
+    time: "TBD",
+    topics: ["Systems / POH"],
+    status: "Coming Soon",
+  },
+];
+
+export default groundSchoolSessions;
